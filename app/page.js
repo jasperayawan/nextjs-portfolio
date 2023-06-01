5ec0a6bacfc2a377eb39edcdd2ce6d482b832c5c
jasperayawan <ejayawan22@gmail.com>
2023-06-02 00:05:57 +0800

    update header

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home(){
   return(
     <main>
-      
+      home
     </main>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,19 @@
+'use client'
+
 import Link from 'next/link';
 import Image from 'next/image'
 
 export default function Nav(){
     return(
         <nav className="flex justify-between f-full">
-            <Link>
-
+            <Link href="/">
+                <Image
+                    src='/assets/images/jasko.jpg'
+                    alt='my profile'
+                    width={50}
+                    height={50}
+                    className='rounded-full'
+                />
             </Link>
         </nav>
     )
