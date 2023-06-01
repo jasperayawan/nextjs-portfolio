7b2998368518c2b81c5999df4c3051f11cdfb194
jasperayawan <ejayawan22@gmail.com>
2023-06-01 23:51:59 +0800

    initial update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,12 @@
-import {Link} from 'next/link';
+import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Nav(){
     return(
         <nav className="flex justify-between f-full">
-            
+            <Link>
+
+            </Link>
         </nav>
     )
 }
