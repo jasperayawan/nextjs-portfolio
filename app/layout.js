03af42adddd643dd30a22f9f7e4ca6459c20c0c1
jasperayawan <ejayawan22@gmail.com>
2023-06-02 01:59:54 +0800

    update gitignore

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
 import '@styles/globals.css'
 import Nav from '@components/Nav';
+import Head from 'next/head'
 
 export const metadata = {
   title: 'Jasper Ayawan - Software Engineer',
-  description: 'Im an aspiring software engineer and currently 4th year college IT student at Southern Mindanao Colleges',
+  description: "I'm an aspiring software engineer and currently 4th year college IT student at Southern Mindanao Colleges",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name='descrition' content={metadata.description}/>
+      </Head>
       <body>
         <main className='app'>
           <Nav />
