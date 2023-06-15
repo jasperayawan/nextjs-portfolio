e2783c42d7733ca6586e0adad9f0065af190e1d8
jasperayawan <ejayawan22@gmail.com>
2023-06-15 13:54:14 +0800

    update contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,38 +1,138 @@
+import { useState } from "react";
+import { Button, FormControl,FormErrorMessage,FormLabel,Heading,Input, Textarea, Text, useToast } from '@chakra-ui/react'
+import { sendContactForm } from "@lib/api";
+
+const initValues = { name: "", email: "", subject: "", message: "" }
+
+const initState = {values: initValues}
+
 export default function Contact() {
+  const toast = useToast()
+  const [state, setState] = useState(initState)
+  const [touched, setTouched] = useState({})
+
+  const {values, isLoading, error} = state;
+
+  const onBlur = ({target}) => setTouched((prev) => ({...prev,
+    [target.name]:true
+  }))
+
+  const handleChange = ({ target }) =>  setState((prev) => ({
+    ...prev, 
+    values: {
+      ...prev.values,
+      [target.name]: target.value,
+    },
+  }));
+
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading:true
+    }));
+
+    try{
+      await sendContactForm(values);
+      setTouched({})
+      setState(initState);
+      toast({
+        title: "Message sent.",
+        status: "Success",
+        duration: 2000,
+        position: "top",
+      })
+    }
+    catch(error){
+      setState((prev) => ({
+        ...prev,
+        isLoading:false,
+        error:error.message
+      }));
+    }
+  }
+
   return (
     <section className="min-h-screen flex flex-col justify-center items-center" id="contact">
       <div className="w-full">
         <div className="__title mb-4 text-center">Contact us</div>
         <div className="max-w-lg mx-auto">
-          <form method="POST" className="w-full">
-            <div className="flex flex-col gap-2">
-              <label>Name</label>
-              <input
+          {error && (
+            <Text color="red.300" my={4} fontSize="xl">
+              {error}
+            </Text>
+          )}
+          
+          <FormControl isRequired isInvalid={touched.name && !values.name} mb={5}>
+              <FormLabel>Name</FormLabel>
+              <Input
                 type="text"
                 placeholder="Enter your name"
-                className="ring-1 ring-zinc-400 pl-2 rounded-md py-1"
+                name="name"
+                value={values.name}
+                errorBorderColor="red.300"
+                onChange={handleChange}
+                onBlur={onBlur}
+                className="ring-1 ring-zinc-400 pl-2 rounded-md py-1 w-full"
                 required
               />
-              <label>Email</label>
-              <input
+              <FormErrorMessage>Required</FormErrorMessage>
+          </FormControl>
+        
+          <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
+              <FormLabel>Email</FormLabel>
+              <Input
                 type="email"
+                name="email"
+                value={values.email}
+                errorBorderColor="red.300"
+                onChange={handleChange}
+                onBlur={onBlur}
                 placeholder="Enter your email"
-                className="ring-1 ring-zinc-400 pl-2 rounded-md py-1"
+                className="ring-1 ring-zinc-400 pl-2 rounded-md py-1 w-full"
                 required
               />
-              <label>Message</label>
-              <textarea
+              <FormErrorMessage>Required</FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={touched.subject && !values.subject} mb={5}>
+              <FormLabel>Subject</FormLabel>
+              <Input
+                type="text"
+                name="subject"
+                value={values.subject}
+                errorBorderColor="red.300"
+                onChange={handleChange}
+                onBlur={onBlur}
+                placeholder="Enter your subject"
+                className="ring-1 ring-zinc-400 pl-2 rounded-md py-1 w-full"
+                required
+              />
+              <FormErrorMessage>Required</FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={touched.message && !values.message} mb={5}>
+              <FormLabel>Message</FormLabel>
+              <Textarea
                 name="message"
                 id=""
                 cols="30"
-                rows="10"
-                className="ring-1 ring-zinc-400 rounded-md p-2"
+                rows={4}
+                value={values.message}
+                errorBorderColor="red.300"
+                onChange={handleChange}
+                onBlur={onBlur}
+                className="ring-1 ring-zinc-400 rounded-md p-2 w-full"
                 placeholder="message"
-              ></textarea>
-              <button type="submit" className="__sbmt">Submit</button>
+              ></Textarea>
+              <FormErrorMessage>Required</FormErrorMessage>
+              <div className="flex justify-center items-start">
+                <Button 
+                  type="submit" 
+                  isLoading={isLoading}
+                  disabled={!values.name || !values.email || !values.subject || !values.message}
+                  onClick={onSubmit} 
+                  className="__sbmt px-4">Submit</Button>
+              </div>
+              </FormControl>
             </div>
-          </form>
-        </div>
       </div>
     </section>
   );
---
+++ config/nodemailer.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer'
+
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: email,
+        pass,
+    }
+})
+
+
+export const mailOptins = {
+    from:email,
+    to: email,
+}
